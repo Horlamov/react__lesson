f85c03d9f7442af44ac43f9fdc451fe7058454d2
Horlamov <XoMA32@yandex.ru>
2021-01-28 00:04:44 +0300

    Lesson React

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = () => {
+return ( <header className='header'>
+	<img src='https://www.pngkey.com/png/full/983-9832079_logo-torrblog-wilkes-barre-area-wolfpack.png' />
+</header>
+)
+};
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Profile = () => {
+  return ( <div className='content'>
+    <div>
+      <img
+        src='https://yandex-images.naydex.net/XM5a12C17/49f80e0g/TXsNIXMROXbSebUig3ejRTg9II0wlY-ReTaZOn7y86XoQ_wbjwVLKxy7EY2jrjm-5ImB4I4wRt2-0NOmrKJooRlqSWHy8OwyIl11ZufAtsUcLIRdMGCK5b2zDO5vVxZhzNJpXvy1byoYfiTI5Q5pzqV7c0K_1zVKaLcBwW4mnbHsel2QNsSalEZqxrOwkCH02dkSTRPnn2HMhhn4WVIQ5Q3yPum8ZgjqaC8FaKQb3ijwWuDxPmR0-7qlspNuRoyhTMiOxOX1m8cAy6Yz8LODREhJAX1iJk3hb4bbqlgicTZ8cP95_bQ42Tk_VVgGiyz6gIpAM_7X9oq79UIjrieuIGwK_3aF1Zh10WqlwxMi4vT6aPI_4zU-Ul3mSanbMxNkXcCvOE6XuVop7lYYB_mMKxKqslDPJHWLqbThMwyWfyNumR4mJWWLR1L511OwUXPkCGvxnYBVPJB_pyobKsJBx92SjKrvxCnL2rx0e4bZ7Ztgy5DAbeeUG-uHYUBu5g0THrg_RAenSbaCK7fxs2JClBvY0HwjJEygTTdZO8rwwwZPspyKTdRKqLou1soH2a354upi8-3VRAhZJ7FxzmY9An6LHnYVt3gk48rl0eFSYJf7WFAPgXRN477mqZtqkFKXzIF92my3SfnLfIc5JXte2YLI0lCN9-Z4ePajEZ-WPJAveKwGdvcIdBMIx3GysfKVqzmhPTAVXtOfxvo7-3CT5w4Sbznc1gnqKX7XKGX5vPqSKFJg_3VmezrHMRNttA1TL1ivBlcnSAQiWmWC8KMhJRk7Uc1DRp5SLJabq6mQIpbt0wyIDCc7m9hfZNhGeE-KwCoTs81nBos41XLzbLddQCz6HsR2hro2EltUQLAQUxYpuFBsMLc_8MxX-YsZEZFkLbIPGS7FWziL7NT6ZCruKoDpoZLtRDUrO1TgML6mH5AuuN03p6d5tRBZlcLicmCnSdkQLDCHP6AMdJnYGWMCY'></img>
+    </div>
+    <div>ava+description</div>
+    <div>my post
+      <div>
+        New post
+      </div>
+      <div>
+        <div>Post 1</div>
+        <div>Post 2</div>
+      </div>
+    </div>
+  </div>
+  )
+};
+
+export default Profile;
