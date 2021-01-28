5e04d244f99a2e6271f0f4965ba31d363d5a0b9c
Horlamov <XoMA32@yandex.ru>
2021-01-28 23:19:53 +0300

    Lesson about css modules in react

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import s from '../css/Header.module.css';
 
 const Header = () => {
-return ( <header className='header'>
+return ( <header className={s.header}>
 	<img src='https://www.pngkey.com/png/full/983-9832079_logo-torrblog-wilkes-barre-area-wolfpack.png' />
 </header>
 )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
+import s from '../css/Navbar.module.css';
 
 const Navbar = () => {
-  return ( <nav className='nav'>
-        <div>
-          <a href='#'>Profile</a>
+  return ( <nav className={s.nav}>
+        <div className={s.item}>
+          <a >Profile</a>
         </div>
-        <div>
-          <a href='#'>Messages</a>
+        <div className={s.item}>
+          <a >Messages</a>
         </div>
-        <div>
-          <a href='#'>Music</a>
+        <div className={s.item}>
+          <a >Music</a>
         </div>
-        <div>
+        <div className={s.item}>
           <a href='#'>News</a>
         </div>
-        <div>
+        <div className={s.item}>
           <a href='#'>Settings</a>
         </div>
       </nav>
