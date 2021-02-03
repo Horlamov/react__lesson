e250f0517841821c1000fb97126a59018471ec31
Horlamov <XoMA32@yandex.ru>
2021-02-03 21:32:18 +0300

    Lesson 21. Create front and style dialogs page

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,10 +1,34 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import s from './Dialogs.module.css';
 
 const Dialogs = (props) => {
   return (
-    <div>
-      Dialogs
+
+    <div className={s.dialogs}>
+      <div className={s.dialogsitems}>
+        <div className={s.dialog + ' ' + s.activ}>
+          <NavLink to=''>Dima </NavLink>
+        </div>
+        <div className={s.dialog}>
+          <NavLink to=''> Andrey </NavLink>
+        </div>
+        <div className={s.dialog}>
+          <NavLink to=''>Iron </NavLink>
+        </div>
+        <div className={s.dialog}>
+          <NavLink to=''>Petr </NavLink>
+        </div>
+        <div className={s.dialog}>
+          <NavLink to=''>Gena </NavLink>
+        </div>
+      </div>
+
+      <div className={s.messages}>
+        <div className={s.message}>Hi</div>
+        <div className={s.message}>How is your life?</div>
+        <div className={s.message}>WHATSUP!!!</div>
+      </div>
     </div>
   )
 };
